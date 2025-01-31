0f2e1792e4ef726260fafc8b7bc30e035fe7b9dc
Akram Ali <akram.exp02@gmail.com>
2025-01-31 21:14:10 +0530

    minor changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,19 @@
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/ui/BookList";
 import { Button } from "@/components/ui/button";
+import { sampleBooks } from "@/constants";
 import React from "react";
 
 const Home = () => {
   return (
     <>
-      <BookOverview />
+      <BookOverview {...sampleBooks[0]} />
 
-      <BookList />
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
     </>
   );
 };
